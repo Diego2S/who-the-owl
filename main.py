e2f0fa600863e71a8ed06da04bf5b2bac87f849d
Diego2S <limao.smart@gmail.com>
2025-12-08 22:42:49 -0300

    setting limits of 'who'

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ bg = Actor("background_800x600") # type: ignore
 
 who = Actor("who2") 
 who.x = 400
-who.y = 600
+who.y = 550
 
 candy = Actor("candy2")
 candy.x = random.randint(20,780)
@@ -37,5 +37,16 @@ def draw():
     bomb.draw()
     ant.draw()
 
+def update():
+    # mover owl
+    if keyboard.left:
+        who.x -= 5
+    if keyboard.right:
+        who.x += 5
+
+    if who.x < 60:
+        who.x = 60
+    if who.x > 740:
+        who.x = 740
 
 pgzrun.go()
